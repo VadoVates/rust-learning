3a3f7b25b4471ca4cadca9ff45d7dbfebcb3cdb9
VadoVates <121289099+VadoVates@users.noreply.github.com>
2023-05-24 22:12:20 +0200

    more learning

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Zgadnij liczbę");
+
+    let secret_number = rand::thread_rng().gen_range(1..100);
+
+    println!("Secret number is: {secret_number}");
 
     println!("Wprowadź liczbę o której myślisz");
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess).expect("Błąd odczytu");
+    let guess: i32 = guess.trim().parse().expect("Wpisz liczbę naturalną cymbale");
 
-    println!("Zgadłeś: {guess}");
+    println!("Wybrałeś: {guess}");
+
+    match guess.cmp(&secret_number)
+    {
+        Ordering::Less => println!("Za mało"),
+        Ordering::Equal => println!("Git, zgadnięte"),
+        Ordering::Greater => println!("Za dużo"),
+    }
 }
