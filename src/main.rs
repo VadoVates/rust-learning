0ee2228b275e42397da10a2bbd614b4246c9c1ba
VadoVates <121289099+VadoVates@users.noreply.github.com>
2023-05-25 07:18:03 +0200

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Zgadnij liczbę");
@@ -9,18 +9,26 @@ fn main() {
 
     println!("Secret number is: {secret_number}");
 
-    println!("Wprowadź liczbę o której myślisz");
-    let mut guess = String::new();
+    loop {
+        println!("Wprowadź liczbę o której myślisz");
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Błąd odczytu");
-    let guess: i32 = guess.trim().parse().expect("Wpisz liczbę naturalną cymbale");
+        io::stdin().read_line(&mut guess).expect("Błąd odczytu");
+        let guess: u32 = match guess.trim().parse()
+            {
+                Ok(num) => num,
+                Err (_) => {println!("Miała być liczba, debilu"); continue;}
+            };
 
-    println!("Wybrałeś: {guess}");
+        println!("Wybrałeś: {guess}");
 
-    match guess.cmp(&secret_number)
-    {
-        Ordering::Less => println!("Za mało"),
-        Ordering::Equal => println!("Git, zgadnięte"),
-        Ordering::Greater => println!("Za dużo"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Za mało"),
+            Ordering::Equal => {
+                println!("Git, zgadnięte");
+                break;
+            }
+            Ordering::Greater => println!("Za dużo"),
+        }
     }
 }
